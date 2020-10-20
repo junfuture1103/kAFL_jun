b325ee3a92ad50f2e2d048bb71b11acc0cbc4a97
Juhyun <thegreatsonga@gmail.com>
2020-10-20 10:49:29 +0900

    Update winfo.py (typo)

--- winfo.py
+++ winfo.py
@@ -29,7 +29,7 @@ with open(SRC, 'r') as f:
 res = subprocess.check_output([
     './kAFL-Fuzzer/kafl_info.py',
     '-vm_dir', './snapshot_win/',
-    '-vm_ram', './snapshot_win/'
+    '-vm_ram', './snapshot_win/',
     '-agent', './targets/windows_x86_64/bin/info/info.exe',
     '-mem', '4096',
     '-v',
